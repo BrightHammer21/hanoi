183339925081bdca80101d9a0c6ad83c5cc15eb9
BrightHammer21 <lucieboucquemont@hotmail.fr>
2020-12-09 15:07:59 +0100

    Lucie Boucquemont

--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -6,9 +6,64 @@ import org.junit.Test;
 
 public class TourTest {
 
-	@Test
-	public void test() {
+	Disque d1 = new Disque(1);
+	Disque d2 = new Disque(2);
+	Disque d3 = new Disque(3);
 
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void testEmpiler_TourPleine_False() {
+
+		Tour tourPleine = new Tour(1);
+		tourPleine.empiler(d2);
+
+		boolean expected = false;
+		boolean actual = tourPleine.empiler(d1);
+
+		assertEquals(expected,actual);
 	}
 
-}
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void testEmpiler_TourNonVideDisquePlusPetit_TRUE() {
+
+		Tour tourNonVide = new Tour(3);
+		tourNonVide.empiler(d2);
+
+		boolean expected = true;
+		boolean actual = tourNonVide.empiler(d1);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void testEmpiler_TourVide_TRUE() {
+		Tour tourVide = new Tour(3);
+
+		boolean expected = true;
+		boolean actual = tourVide.empiler(d1);
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void testEmpiler_TourEstPleine_TRUE() {
+
+		int expected = 3;
+
+		//assertEquals(expected,d3);
+	}
+
+
+
+	}
